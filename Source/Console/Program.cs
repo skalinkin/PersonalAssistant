2641091fa1823ef1fd4f36a7cbbb41b3dc4bfca9
Sergiy Kalinkin <skalinkin@gmail.com>
2016-01-28 09:34:04 -0500

    init

---
+++ Source/Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using PersonalAssistant;
+using PersonalAssistant.InMemmoryStore;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bootstrapper.InitializeBuilder();
+            Bootstrapper.Builder.RegisterModule<InMemmoryStoreModule>();
+
+        }
+    }
+}
