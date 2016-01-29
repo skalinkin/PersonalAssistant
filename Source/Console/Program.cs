67cdf0a66d8c0644e10839ad941a5cac0d85fc4f
Sergiy Kalinkin <skalinkin@gmail.com>
2016-01-29 09:21:37 -0500

    init

--- Source/Console/Program.cs
+++ Source/Console/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Autofac;
 using PersonalAssistant;
+using PersonalAssistant.Craigslist;
 using PersonalAssistant.InMemmoryStore;
 
 namespace Console
@@ -15,7 +16,27 @@ namespace Console
         {
             Bootstrapper.InitializeBuilder();
             Bootstrapper.Builder.RegisterModule<InMemmoryStoreModule>();
+            Bootstrapper.Builder.RegisterModule<CraigslistModule>();
 
+            Bootstrapper.SetAutofacContainer();
+            var feed = Bootstrapper.Container.Resolve<IOpportunityFeed>();
+            var repository = Bootstrapper.Container.Resolve<IOpportunityRepository>();
+
+            var result = feed.Fetch();
+
+            foreach (var opportunity in result)
+            {
+                repository.Save(opportunity);
+            }
+
+            var opportunities = repository.GetAll().ToArray();
+
+            foreach (var opportunity in opportunities)
+            {
+                System.Console.WriteLine(opportunity.Title);
+            }
+
+            System.Console.ReadLine();
         }
     }
 }
