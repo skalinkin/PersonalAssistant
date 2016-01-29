3083867d3d300ec2c9441f5e257eebf7cb8103c5
Sergiy Kalinkin <skalinkin@gmail.com>
2016-01-29 13:41:25 -0500

    init

--- Source/Console/Program.cs
+++ Source/Console/Program.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
+﻿using Autofac;
 using PersonalAssistant;
 using PersonalAssistant.Craigslist;
 using PersonalAssistant.InMemmoryStore;
 
 namespace Console
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Bootstrapper.InitializeBuilder();
             Bootstrapper.Builder.RegisterModule<InMemmoryStoreModule>();
@@ -22,16 +17,14 @@ namespace Console
             var feed = Bootstrapper.Container.Resolve<IOpportunityFeed>();
             var repository = Bootstrapper.Container.Resolve<IOpportunityRepository>();
 
-            var result = feed.Fetch();
+            var result = feed.FetchNew();
 
             foreach (var opportunity in result)
             {
                 repository.Save(opportunity);
             }
 
-            var opportunities = repository.GetAll().ToArray();
-
-            foreach (var opportunity in opportunities)
+            foreach (var opportunity in repository.FindAll())
             {
                 System.Console.WriteLine(opportunity.Title);
             }
